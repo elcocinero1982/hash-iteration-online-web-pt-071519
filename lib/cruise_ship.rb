68dcd0ca6332c97d43197774ac0c2dff77fed03f
jose luis <cortezluisjose@gmail.com>
2019-07-30 04:23:46 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,6 +12,11 @@ suite_e: "Crumpet the Elf"
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(passengers)
+def winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+endselect_winner(passengers)
   # add the code snippet here!
 end
